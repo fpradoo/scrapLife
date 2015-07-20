423ee74aab4c1f02eec918938442c58e20940d94
fpradoo <fer1588@hotmail.com>
2015-07-20 15:04:54 -0300

    cambio de estructura

---
+++ admin/includes/navigation.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-defaut navbar-fixed-top">
+	<div class="container">
+		<a href="/index.php" class="navbar-brand"> Scrap Life</a>
+		<ul class="nav navbar-nav">
+			<li><a href="categorias.php">Categorias</a></li>
+		</ul>
+	</div>
+</nav>
---
+++ admin/index.php
@@ -0,0 +1,10 @@
+<?php
+	require_once'../core/init.php';
+	include 'includes/head.php';
+	include 'includes/navigation.php';
+?>
+
+Backend
+<?php
+	include 'includes/footer.php';
+?>
